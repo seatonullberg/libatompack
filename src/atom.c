d887bbf0ae0f06ad3654deeb362f7e37477e22f8
Seaton Ullberg <seatonullberg@gmail.com>
2020-06-16 10:58:40 -0400

    Fix function parameters

--- src/atom.c
+++ src/atom.c
@@ -1,13 +1,33 @@
+#include <string.h>
+
 typedef struct
 {
     int id;
     double position[3];
 } Atom;
 
+Atom atom_new(int id, double position[3])
+{
+    Atom atom;
+    memcpy(&atom.id, &id, sizeof(int));
+    memcpy(&atom.position, position, sizeof(double) * 3);
+    return atom;
+}
+
 typedef struct
 {
-    Atom *atoms;
     double basis[3][3];
-    int capacity;
-    int size;
+    size_t size;
+    size_t capacity;
+    Atom *atoms[];
 } AtomCollection;
+
+AtomCollection atom_collection_new(double basis[3][3], size_t size, size_t capacity, Atom *atoms[])
+{
+    AtomCollection collection;
+    memcpy(&collection.basis, basis, sizeof(double) * 3 * 3);
+    memcpy(&collection.size, &size, sizeof(size_t));
+    memcpy(&collection.capacity, &capacity, sizeof(size_t));
+    memcpy(&collection.atoms, atoms, sizeof(Atom *));
+    return collection;
+}
--- src/util.c
+++ src/util.c
@@ -1,56 +1,52 @@
 #include <math.h>
+#include <string.h>
 
-double **metric_tensor(double a, double b, double c, double alpha, double beta, double gamma)
+void metric_tensor(double a, double b, double c, double alpha, double beta, double gamma, double out[3][3])
 {
-    double m[3][3] = {
-        {a * a, a * b * cos(gamma), a * c * cos(beta)},
-        {a * b * cos(gamma), b * b, b * c * cos(alpha)},
-        {a * c * cos(beta), b * c * cos(alpha), c * c},
-    };
-    return &m;
+    out[0][0] = a * a;
+    out[0][1] = a * b * cos(gamma);
+    out[0][2] = a * c * cos(beta);
+    out[1][0] = a * b * cos(gamma);
+    out[1][1] = b * b;
+    out[1][2] = b * c * cos(alpha);
+    out[2][0] = a * c * cos(beta);
+    out[2][1] = b * c * cos(alpha);
+    out[2][2] = c * c;
 }
 
-double **polyhedron_vertices(double **poly)
+void polyhedron_vertices(double poly[3][3], double out[8][3])
 {
-    double m[8][3] = {
-        {0.0, 0.0, 0.0}, // origin
-        *poly[0],        // x corner
-        *poly[1],        // y corner
-        *poly[2],        // z corner
-        {
-            poly[0][0] + poly[1][0],
-            poly[0][1] + poly[1][1],
-            poly[0][2] + poly[1][2],
-        }, // xy corner
-        {
-            poly[0][0] + poly[2][0],
-            poly[0][1] + poly[2][1],
-            poly[0][2] + poly[2][2],
-        }, // xz corner
-        {
-            poly[1][0] + poly[2][0],
-            poly[1][1] + poly[2][1],
-            poly[1][2] + poly[2][2],
-        }, // yz corner
-        {
-            poly[0][0] + poly[1][0] + poly[2][0],
-            poly[0][1] + poly[1][1] + poly[2][1],
-            poly[0][2] + poly[1][2] + poly[2][2],
-        }, // xyz corner
+    double origin[3] = {0.0, 0.0, 0.0};
+    double x[3] = {poly[0][0], poly[0][1], poly[0][2]};
+    double y[3] = {poly[1][0], poly[1][1], poly[1][2]};
+    double z[3] = {poly[2][0], poly[2][1], poly[2][2]};
+    double xy[3] = {
+        poly[0][0] + poly[1][0],
+        poly[0][1] + poly[1][1],
+        poly[0][2] + poly[1][2],
     };
-    return &m;
-}
-
-double **polyhedron_faces(double **poly)
-{
-    double **verts = polyhedron_vertices(poly);
-    double m[6][3] = {
-        {*verts[4], *verts[1], *verts[2]}, // lower xy
-        {*verts[7], *verts[6], *verts[5]}, // upper xy
-        {*verts[5], *verts[3], *verts[1]}, // rear xz
-        {*verts[2], *verts[6], *verts[4]}, // front xz
-        {*verts[6], *verts[2], *verts[3]}, // left yz
-        {*verts[1], *verts[4], *verts[5]}, // right yz
+    double xz[3] = {
+        poly[0][0] + poly[2][0],
+        poly[0][1] + poly[2][1],
+        poly[0][2] + poly[2][2],
     };
-    return &m;
+    double yz[3] = {
+        poly[1][0] + poly[2][0],
+        poly[1][1] + poly[2][1],
+        poly[1][2] + poly[2][2],
+    };
+    double xyz[3] = {
+        poly[0][0] + poly[1][0] + poly[2][0],
+        poly[0][1] + poly[1][1] + poly[2][1],
+        poly[0][2] + poly[1][2] + poly[2][2],
+    };
+    size_t row_size = sizeof(double) * 3;
+    memcpy(out[0], &origin, row_size);
+    memcpy(out[1], &x, row_size);
+    memcpy(out[2], &y, row_size);
+    memcpy(out[3], &z, row_size);
+    memcpy(out[4], &xy, row_size);
+    memcpy(out[5], &xz, row_size);
+    memcpy(out[6], &yz, row_size);
+    memcpy(out[7], &xyz, row_size);
 }
